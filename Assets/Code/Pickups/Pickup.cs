f58e6363c239a100f5be395b3598bc8b83ad7c4c
Mathias Bevers <mathiasbevers@outlook.com>
2023-05-11 20:08:33 +0200

    Added very basic pickup behaviour
    - Removed the scriptable object approach
    - Added a menu item to create prefabs

--- Assets/Code/Pickups/Pickup.cs
+++ Assets/Code/Pickups/Pickup.cs
@@ -1,6 +1,17 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Pickup : MonoBehaviour
 {
-	[field: SerializeField] public PickupData data { get; private set; } 
+	[Flags] public enum Target { Self = 1, Opponent = 1 << 1 }
+
+	[field: SerializeField] public Target target { get; private set; } = Target.Opponent;
+	[SerializeField] public UnityEvent onPickup;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//TODO: add player check
+		onPickup.Invoke();
+	}
 }
