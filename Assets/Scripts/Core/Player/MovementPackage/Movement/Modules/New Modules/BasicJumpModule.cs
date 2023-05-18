085b2b65e9a5357ae0f1f1ca006e674f3eb7dc75
Amber Kortier <AmberKortier@gmail.com>
2023-05-18 19:52:13 +0200

    Single turns allowed

--- Assets/Scripts/Core/Player/MovementPackage/Movement/Modules/New Modules/BasicJumpModule.cs
+++ Assets/Scripts/Core/Player/MovementPackage/Movement/Modules/New Modules/BasicJumpModule.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[PreferComponent(typeof(CooldownSharer))]
 public class BasicJumpModule : MotorModule
 {
 
@@ -13,28 +14,41 @@ public class BasicJumpModule : MotorModule
 
     [SerializeField] KeyCode debugJumpKey;
 
+    bool jumpedThisFrame = false;
+
+    CooldownSharer sharer;
+
+    protected override void OnAwake()
+    {
+        sharer = GetComponent<CooldownSharer>();
+    }
+
     //STRICKTLY FORBIDDEN TO USE IT FOR MOVEMENT!!!!!!!!
     //BUT YOU CAN USE IT FOR TIMERS!
     private void Update()
     {
-        if (Input.GetKeyDown(debugJumpKey) && allowCoyoteTime)
+        timer -= Time.deltaTime;
+        if (Input.GetKeyDown(debugJumpKey) && allowCoyoteTime && !jumpedThisFrame)
             timer = coyoteSeconds;
+        jumpedThisFrame = false;
     }
 
     public override void OnUpdate(MotorState state)
     {
         if (Input.GetKeyDown(debugJumpKey))
             timer = coyoteSeconds;
+        jumpedThisFrame = false;
 
         if (timer > 0)
             Jump();
-        timer -= Time.deltaTime;
+
     }
 
     [@Button]
     public void Jump()
     {
         timer = 0;
+        jumpedThisFrame = true;
         registry.ResetYVel();
         registry.AddVelocity(new Vector3(0, Mathf.Sqrt(jumpHeight), 0));
     }
