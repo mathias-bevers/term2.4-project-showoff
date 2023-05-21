d7d531b0aa1392f76f82d0e0eb515f995bd8efd2
Amber Kortier <AmberKortier@gmail.com>
2023-05-21 18:35:29 +0200

    Moved around Map Generation Files

---
+++ Assets/Scripts/Core/MapGeneration/New Map Generator/Scriptable Objects/MapGroup.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/Map Generation/Map Group", fileName = "MapGroup", order = 11)]
+public class MapGroup : ScriptableObject
+{
+    
+}
