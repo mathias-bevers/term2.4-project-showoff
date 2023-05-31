44b8833ae9be3e3a37a7e95538eb2cdd6476c887
Mathias <mathiasbevers@outlook.com>
2023-05-31 12:33:01 +0200

    startup filtering bad clients

--- Assets/Code/Networking/Server.cs
+++ Assets/Code/Networking/Server.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using NaughtyAttributes;
@@ -8,8 +9,11 @@ using UnityEngine;
 public class Server : Singleton<Server>
 {
 	private bool isRunning;
+
+	private Dictionary<int, TcpClient> clients;
 	private int currentID = -1;
-	private List<TcpClient> clients;
+	private List<int> badClients;
+
 	private TcpListener listener;
 
 
@@ -18,13 +22,14 @@ public class Server : Singleton<Server>
 		if (!isRunning) { return; }
 
 		ProcessNewClients();
+		ProcessBadClients();
 		ProcessExistingClients();
 	}
 
 	public void Initialize(IPAddress ip, int port)
 	{
 		listener = new TcpListener(ip, port);
-		clients = new List<TcpClient>(2);
+		clients = new Dictionary<int, TcpClient>(2);
 		listener.Start();
 
 		isRunning = true;
@@ -43,37 +48,58 @@ public class Server : Singleton<Server>
 			{
 				TcpClient rejected = listener.AcceptTcpClient();
 				packet.Write(false);
-				WriteToClient(rejected, packet);
+				WriteToClient((-1, rejected), packet);
 				rejected.Close();
 				Debug.LogWarning("Refused client, server is full");
 				continue;
 			}
 
 			TcpClient accepted = listener.AcceptTcpClient();
-			clients.Add(accepted);
 			++currentID;
+			clients.Add(currentID, accepted);
 
 			packet.Write(true);
 			packet.Write(currentID);
-			WriteToClient(accepted, packet);
+			WriteToClient((currentID, accepted), packet);
 			Debug.Log($"Accepted new client with id: {currentID}");
 		}
 	}
 
 	private void ProcessExistingClients()
 	{
-		foreach (TcpClient client in clients)
+		foreach (KeyValuePair<int, TcpClient> idClient in clients)
 		{
-			if (client.Available == 0) { continue; }
+			if (idClient.Value.Available == 0) { continue; }
 
-			byte[] inBytes = StreamUtil.Read(client.GetStream());
+			byte[] inBytes = StreamUtil.Read(idClient.Value.GetStream());
+
+			foreach (KeyValuePair<int, TcpClient> idReceiver in clients.Where(c => c.Key != idClient.Key))
+			{
+				Packet packet = new(inBytes);
+				WriteToClient((idClient.Key, idReceiver.Value), packet);
+			}
 		}
 	}
 
-	private void WriteToClient(TcpClient receiver, Packet data)
+	private void ProcessBadClients()
 	{
-		//TODO: add try catch etc
-		StreamUtil.Write(receiver.GetStream(), data.GetBytes());
+		//TODO: Add heartbeat.
+
+		if (badClients.Count < 1) { return; }
+
+		foreach (int badClientID in badClients) { clients.Remove(badClientID); }
+
+		badClients.Clear();
+	}
+
+	private  void WriteToClient((int, TcpClient) idReceiver, Packet data)
+	{
+		try { StreamUtil.Write(idReceiver.Item2.GetStream(), data.GetBytes()); }
+		catch (System.IO.IOException)
+		{
+			Debug.Log($"Marking client#{idReceiver.Item1}");
+			badClients.Add(idReceiver.Item1);
+		}
 	}
 
 #if UNITY_EDITOR
