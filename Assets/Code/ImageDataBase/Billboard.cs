9d12e57fe853da9c871a79c6d7a3fb4807bc49c9
Mathias Bevers <mathiasbevers@outlook.com>
2023-06-22 19:17:19 +0200

    Prototype as of week 7
    - Added a level element that contains the billboard prefab. This is a copy of the straight element piece.
    - Added textures to the billboard prefab.
    - Edited the `ModelHelper` class to handle billboard canvases.

--- Assets/Code/ImageDataBase/Billboard.cs
+++ Assets/Code/ImageDataBase/Billboard.cs
@@ -6,11 +6,27 @@ public class Billboard : MonoBehaviour
 {
 	public event Action<Billboard> destroyingEvent;
 
+	private readonly float[] possibleRotations =
+	{
+		-120.0f,
+		0.0f,
+		120.0f,
+	};
 
 	[field: SerializeField] public Image[] images { get; private set; }
 	public string[] displayingImageNames { get; private set; }
 	
-	private void Start() { displayingImageNames = BillboardManager.Instance.RequestSetup(this); }
+	private Transform cachedTransform;
+
+	private void Start()
+	{
+		cachedTransform = transform;
+		
+		float yRotation = cachedTransform.rotation.y + possibleRotations.GetRandomElementStruct(); 
+		cachedTransform.rotation = Quaternion.Euler(0, yRotation, 0);
+		
+		displayingImageNames = BillboardManager.Instance.RequestSetup(this);
+	}
 
 	private void OnDestroy()
 	{
